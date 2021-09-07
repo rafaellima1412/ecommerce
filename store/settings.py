96e0110c4d0800d232afbf6385a4ca8f2f821bcd
rafaellima1412 <rafaellima1412@gmail.com>
2021-09-07 14:33:36 -0400

    add tests

--- store/settings.py
+++ store/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    # minhas apps
+    "users.apps.UsersConfig",
 ]
 
 MIDDLEWARE = [
@@ -108,9 +110,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.1/topics/i18n/
 
-LANGUAGE_CODE = "en-us"
+LANGUAGE_CODE = "pt-br"
 
-TIME_ZONE = "UTC"
+TIME_ZONE = "America/Manaus"
 
 USE_I18N = True
 
@@ -123,3 +125,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = "/static/"
+# User model
+AUTH_USER_MODEL = "users.User"
