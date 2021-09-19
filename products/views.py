d6f76a4d5d4a6d06a8816569fb06f23790470aed
rafaellima1412 <rafaellima1412@gmail.com>
2021-09-19 12:08:14 -0400

    cobertura e filtro

--- products/views.py
+++ products/views.py
@@ -1,5 +1,4 @@
 # from cart.forms import CartAddProductForm
-# from cart.forms import CartAddProductForm
 from django.shortcuts import get_object_or_404
 from django.views.generic import DetailView, ListView
 
@@ -22,7 +21,6 @@ class ProductListView(ListView):
         if category_slug:
             self.category = get_object_or_404(Category, slug=category_slug)
             queryset = queryset.filter(category=self.category)
-
         return queryset
 
     def get_context_data(self, **kwargs):
